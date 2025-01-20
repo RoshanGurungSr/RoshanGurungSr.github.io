728e023c2af11365f393f1c13d8683003e17160a
Roshan Gurung <roshan.gurung.sr@gmail.com>
2025-01-19 21:28:53 -0500

    (refactor): Update the sites

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Kubeflow Setup On Local Machine",
-        "excerpt":"Coming Soon…  ","categories": ["linux","MLOps"],
-        "tags": [],
-        "url": "/linux/mlops/test/",
-        "teaser": null
-      },{
         "title": "Understanding working mechanism of Decision Tree (ID3 Variant)",
         "excerpt":"Coming Soon…  ","categories": ["Machine Learning"],
         "tags": [],
@@ -15,5 +9,11 @@ var store = [{
         "excerpt":"Linear Regression is a statistical model and a supervised learning algorithm used for predicting a continuous target variable based on one or more predictor variables. The target variable is the final output that we are trying to estimate and the predictor variables are the features of the data. The objective...","categories": ["Machine Learning"],
         "tags": [],
         "url": "/machine%20learning/linear_regression/",
+        "teaser": null
+      },{
+        "title": "Highly Scalable Embedding Search with AWS OpenSearch",
+        "excerpt":"Coming Soon…  ","categories": ["Machine Learning"],
+        "tags": [],
+        "url": "/machine%20learning/embedding_search/",
         "teaser": null
       }]
