d7a7e506bf35bc45340555bd975438d264ece37e
Roshan Gurung <roshan.gurung.sr@gmail.com>
2026-01-08 23:30:47 -0500

    :hammer: (feat) Add build files

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,6 +5,12 @@ var store = [{
         "url": "/machine%20learning/decision_tree/",
         "teaser": null
       },{
+        "title": "Understanding working mechanism of Decision Tree (ID3 Variant)",
+        "excerpt":"Coming Soon…  ","categories": ["Machine Learning"],
+        "tags": [],
+        "url": "/machine%20learning/decision_tree/",
+        "teaser": null
+      },{
         "title": "Linear Regression with OLS from scratch",
         "excerpt":"Linear Regression is a statistical model and a supervised learning algorithm used for predicting a continuous target variable based on one or more predictor variables. The target variable is the final output that we are trying to estimate and the predictor variables are the features of the data. The objective...","categories": ["Machine Learning"],
         "tags": [],
@@ -12,13 +18,13 @@ var store = [{
         "teaser": null
       },{
         "title": "Highly Scalable Embedding Search with AWS OpenSearch",
-        "excerpt":"Coming Soon…  ","categories": ["Machine Learning","AWS"],
+        "excerpt":"Coming Soon…  ","categories": ["Machine Learning"],
         "tags": [],
-        "url": "/machine%20learning/aws/embedding_search/",
+        "url": "/machine%20learning/embedding_search/",
         "teaser": null
       },{
-        "title": "Kickstart GenAI with Amazon Bedrock",
-        "excerpt":"Coming Soon…  ","categories": ["Generative AI","AWS"],
+        "title": "Create your own ChatGPT using SageMaker Python SDK V3",
+        "excerpt":"Introduction AWS SageMaker Python SDK V3 has introduced more streamlined and unified core classes for ML workflow by replacing previous framework specific classes. This new SDK has a unified “ModelTrainer” class for handling custom containers and data processing, “ModelBuilder” class for model deployment and inference setup. I didn’t find much...","categories": ["Generative AI","AWS"],
         "tags": [],
         "url": "/generative%20ai/aws/aws_bedrock/",
         "teaser": null
